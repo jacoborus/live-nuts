5deec887b6b08d3490a495156dc98d28f9ef9f69
Jacobo <jacobo@xarope.eu>
2016-01-26 01:05:54 +0100

    refactor get-proxy.js

--- pre/get-proxy.js
+++ pre/get-proxy.js
@@ -1,56 +1,38 @@
 'use strict'
 
-let links
+export default function (links) {
+  let handler = {
+    set (target, prop, value, receiver) {
+      if (typeof value !== 'object') {
+        let link = links.get(receiver)
+        if (link.has(prop)) {
+          link.get(prop).forEach(f => f(value))
+        } else {
+          link.set(prop, new Set())
+        }
+        return Reflect.set(target, prop, value)
+      } else {
+        let p = deepProxy(value)
+        return Reflect.set(target, prop, p)
+      }
+    }
+  }
 
-export default function (inLinks) {
-  links = inLinks
+  function deepProxy (obj) {
+    let sets = [],
+        out = Array.isArray(obj) ? [] : {}
+    Object.keys(obj).forEach(k => {
+      if (typeof obj[k] === 'object') {
+        if (obj[k]) out[k] = deepProxy(obj[k])
+      } else {
+        sets.push(k)
+        out[k] = obj[k]
+      }
+    })
+    let p = new Proxy(out, handler)
+    let link = links.get(p) || links.set(p, new Map()).get(p)
+    sets.forEach(k => !link.has(k) && link.set(k, new Set()))
+    return p
+  }
   return deepProxy
-}
-
-function set (target, prop, value, receiver) {
-  if (typeof value !== 'object') {
-    let link = links.get(receiver)
-    if (link.has(prop)) {
-      link.get(prop).forEach(f => f(value))
-    } else {
-      link.set(prop, new Set())
-    }
-    return Reflect.set(target, prop, value)
-  } else {
-    let p = deepProxy(value)
-    return Reflect.set(target, prop, p)
-  }
-}
-
-let objectHandler = { set }
-let arrayHandler = { set }
-
-function createProxyObject (scope) {
-  if (Array.isArray(scope)) {
-    return new Proxy(scope, arrayHandler)
-  } else {
-    return new Proxy(scope, objectHandler)
-  }
-}
-
-function deepProxy (obj) {
-  let sets = [],
-      out
-  if (Array.isArray(obj)) {
-    out = []
-  } else {
-    out = {}
-  }
-  Object.keys(obj).forEach(k => {
-    if (typeof obj[k] === 'object') {
-      if (obj[k]) out[k] = deepProxy(obj[k])
-    } else {
-      sets.push(k)
-      out[k] = obj[k]
-    }
-  })
-  let p = createProxyObject(out)
-  let link = links.get(p) || links.set(p, new Map()).get(p)
-  sets.forEach(k => !link.has(k) && link.set(k, new Set()))
-  return p
 }
