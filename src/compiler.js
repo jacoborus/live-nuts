edb61ec0950ba5286d66068d8cf41c3d2b95fa52
Jacobo <jacobo@xarope.eu>
2016-02-05 04:52:51 +0100

    add: new method for compile string

--- src/compiler.js
+++ src/compiler.js
@@ -5,6 +5,15 @@ import newCounter from './counter.js'
 let links
 const matcher = /{{([^}]*)}}/
 
+function getRepeatedScope (scope, repeatAtt) {
+  if (repeatAtt) {
+    if (!scope[repeatAtt]) scope[repeatAtt] = []
+    return scope[repeatAtt]
+  } else {
+    return scope
+  }
+}
+
 function getSubscriber (obj) {
   let link = links.get(obj)
   return function (prop, action) {
@@ -12,7 +21,7 @@ function getSubscriber (obj) {
   }
 }
 
-function getScope (scope, scopeAtt, subscribe) {
+function getScopeAndSubscribe (scope, scopeAtt, subscribe) {
   if (scopeAtt) {
     if (!scope[scopeAtt]) {
       scope[scopeAtt] = {}
@@ -29,68 +38,62 @@ function getScope (scope, scopeAtt, subscribe) {
 
 function parseStr (str) {
   if (!str) {
-    return {
-      fns: [],
-      models: new Set()
-    }
+    return []
   }
   let st = str.match(matcher)
   if (st.index) {
     let out = str.substr(0, st.index)
-    let next = parseStr(str.substring(st.index))
-    return {
-      fns: [() => out].concat(next.fns),
-      models: next.models
-    }
+    let fns = parseStr(str.substring(st.index))
+    return [() => out].concat(fns)
   } else {
     let model = st[1].trim()
-    let next = parseStr(str.substring(st[0].length))
-    return {
-      fns: [scope => scope[model] || ''].concat(next.fns),
-      models: next.models.add(model)
-    }
+    let fns = parseStr(str.substring(st[0].length))
+    return [scope => scope[model] || ''].concat(fns)
   }
+}
+
+function compileStr (str) {
+  let fns = parseStr(str)
+  let reduce
+  if (fns.length === 1) {
+    reduce = scope => fns[0](scope)
+  } else {
+    reduce = scope => fns.reduce((str, fn) => str + fn(scope), '')
+  }
+  let models = new Set()
+  str.replace(/{{(.*?)}}/g, (g0, g1) => models.add(g1.trim()))
+  return { reduce, models }
 }
 
 function compileAttributes (atts) { // compile attributes
   let fns = []
-  for (let name in atts) {
+  Object.keys(atts).forEach(name => {
     let value = atts[name]
     if (value.match(matcher)) {
-      let loop = parseStr(value)
+      let { reduce, models } = compileStr(value)
       fns.push(function (el, scope, subscribe) {
         let updateFn = () => {
-          el.setAttribute(name, loop.fns.reduce((str, fn) => str + fn(scope), ''))
+          el.setAttribute(name, reduce(scope))
         }
-        loop.models.forEach(model => subscribe(model, updateFn))
+        models.forEach(model => subscribe(model, updateFn))
         updateFn()
       })
     } else {
       fns.push(el => el.setAttribute(name, value))
     }
-  }
+  })
   return (el, scope, subscribe) => fns.forEach(fn => fn(el, scope, subscribe))
 }
 
 function compileText (schema, callback) {
   let data = schema.data
-  if (schema.data.match(matcher)) {
-    let loop = parseStr(schema.data)
-    if (loop.fns.length === 1) {
-      let fn = loop.fns[0]
-      schema.render = function (scope, subscribe) {
-        let el = document.createTextNode(fn(scope))
-        let updateFn = value => el.textContent = value
-        loop.models.forEach(model => subscribe(model, updateFn))
-        return el
-      }
-    } else {
-      schema.render = function (scope, subscribe) {
-        let el = document.createTextNode(loop.fns.reduce((str, fn) => str + fn(scope), ''))
-        let updateFn = () => el.textContent = loop.fns.reduce((str, fn) => str + fn(scope), '')
-        loop.models.forEach(model => subscribe(model, updateFn))
-        return el
-      }
+  if (data.match(matcher)) {
+    let { reduce, models } = compileStr(data)
+    schema.render = function (scope, subscribe) {
+      let el = document.createTextNode(reduce(scope))
+      let updateFn = () => el.textContent = reduce(scope)
+      models.forEach(model => subscribe(model, updateFn))
+      return el
     }
   } else {
     schema.render = () => document.createTextNode(data)
@@ -105,7 +108,7 @@ function compileTag (schema, callback) {
     renderAttributes = compileAttributes(attribs)
   }
   schema.render = (outerScope, outerSubscribe = getSubscriber(outerScope)) => {
-    let { subscribe, scope } = getScope(outerScope, schema.scope, outerSubscribe),
+    let { subscribe, scope } = getScopeAndSubscribe(outerScope, schema.scope, outerSubscribe),
         el = document.createElement(schema.localName),
         nut = { el, scope }
     // render attributes
@@ -136,7 +139,7 @@ function compileLoop (schema, callback) {
     renderAttributes = compileAttributes(attribs)
   }
   schema.render = (outerScope) => {
-    let scope = getScope(outerScope, schema.scope),
+    let scope = getScopeAndSubscribe(outerScope, schema.scope),
         el = document.createElement(schema.localName),
         nut = { el, scope }
     // render attributes
