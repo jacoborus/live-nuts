3175b1280c1869311278d25c1504ec2c0dcf627d
Jacobo <jacobo@xarope.eu>
2016-02-07 02:36:58 +0100

    add scopes to subscriptions in store

--- src/store-factory.js
+++ src/store-factory.js
@@ -22,19 +22,17 @@ export default function (rosters) {
   }
 
   function deepProxy (obj) {
-    let sets = [],
-        out = Array.isArray(obj) ? [] : {}
+    let out = Array.isArray(obj) ? [] : {}
     Object.keys(obj).forEach(k => {
-      if (typeof obj[k] === 'object') {
-        if (obj[k]) out[k] = deepProxy(obj[k])
+      if (obj[k] && typeof obj[k] === 'object') {
+        out[k] = deepProxy(obj[k])
       } else {
-        sets.push(k)
         out[k] = obj[k]
       }
     })
     let p = new Proxy(out, handler)
-    let link = rosters.get(p) || rosters.set(p, new Map()).get(p)
-    sets.forEach(k => !link.has(k) && link.set(k, new Set()))
+    let link = rosters.set(p, new Map()).get(p)
+    Object.keys(obj).forEach(k => !link.has(k) && link.set(k, new Set()))
     return p
   }
   return deepProxy
