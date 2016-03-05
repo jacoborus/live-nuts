b8bba9f2e96adda5128b8a57dbe05dc549335487
Jacobo <jacobo@xarope.eu>
2016-03-05 03:37:09 +0100

    refactor compileText with cache

--- src/compiler.js
+++ src/compiler.js
@@ -93,14 +93,24 @@ function compileAttributes (schema) { // compile attributes
 function compileText (schema, callback) {
   let data = schema.data
   if (data.match(matcher)) {
+    // text node has scoped content
     let reduce = compileStr(data)
+    let updateFn = (scope, cached, el) => {
+      let fresh = reduce(scope)
+      if (fresh !== cached) {
+        el.textContent = fresh
+        return fresh
+      }
+      return cached
+    }
     schema.render = function (scope, box) {
-      let el = document.createTextNode(reduce(box.get()))
-      let updateFn = () => el.textContent = reduce(box.get())
-      box.subscribe(updateFn, scope)
+      let cached = reduce(scope)
+      let el = document.createTextNode(cached)
+      box.subscribe(() => updateFn(scope, cached, el), scope)
       return el
     }
   } else {
+    // is regular text node
     schema.render = () => document.createTextNode(data)
   }
   callback()
