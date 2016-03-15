c16211e3997890df0f7956fa002efbc44e4fd1be
Jacobo <jacobo@xarope.eu>
2016-03-15 05:02:28 +0100

    add compilers for render requirements

---
+++ src/compilers/requirements.js
@@ -0,0 +1,31 @@
+'use strict'
+
+let reqs = {
+  nuif (schema) {
+    if ('nuif' in schema && 'unless' in schema) {
+      let nuif = schema.nuif,
+          unless = schema.unless
+      return scope => scope[nuif] && !scope[unless]
+    } else if ('nuif' in schema) {
+      let model = schema.nuif
+      return scope => scope[model] ? true : false
+    } else {
+      let model = schema.unless
+      return scope => !scope[model]
+    }
+  },
+
+  model (schema) {
+    let { model } = schema
+    return scope => scope[model] && typeof scope[model] === 'object'
+  },
+
+  modelIf (schema) {
+    let model = schema.model
+    let fn = reqs.model(schema)
+    let fn2 = reqs.nuif(schema)
+    return scope => fn(scope) && fn2(scope[model])
+  }
+}
+
+export default reqs
---
+++ test/compilers/requirements-test.js
@@ -0,0 +1,58 @@
+'use strict'
+
+import test from 'tape'
+import reqs from '../../src/compilers/requirements.js'
+
+test('compile requirements with just if or unless', t => {
+  let schemaIf = {
+    nuif: 't'
+  }
+  let reqif = reqs.nuif(schemaIf)
+  t.ok(reqif({t: 1}), 'true nuif')
+  t.notOk(reqif({t: 0}), 'false nuif')
+
+  let schemaUnless = {
+    unless: 't'
+  }
+  let requnless = reqs.nuif(schemaUnless)
+  t.ok(requnless({t: 0}), 'true unless')
+  t.notOk(requnless({t: 1}), 'false unless')
+
+  let schemaAll = {
+    unless: 'u',
+    nuif: 'i'
+  }
+  let reqall = reqs.nuif(schemaAll)
+  t.ok(reqall({u: 0, i: 1}), 'true all')
+  t.notOk(reqall({u: 1, i: 0}), 'false all')
+  t.notOk(reqall({u: 0, i: 0}), 'false all')
+  t.notOk(reqall({u: 1, i: 1}), 'false all')
+
+  t.end()
+})
+
+test('compile requirements with just model condition', t => {
+  let schema = {
+    model: 'm'
+  }
+  let req = reqs.model(schema)
+  t.ok(req({m: {}}), 'true nuif')
+  t.notOk(req({m: 0}), 'false nuif')
+
+  t.end()
+})
+
+test('compile requirements with nuif and model', t => {
+  let schema = {
+    model: 'm',
+    nuif: 'i'
+  }
+  let req = reqs.modelIf(schema)
+  t.ok(req({m: {i: 1}}), 'true all')
+  t.notOk(req({m: {}, i: 1}), 'false no nuif')
+  t.notOk(req({m: 1, i: 0}), 'false no object as model')
+  t.notOk(req({m: 0, i: 1}), 'false no model')
+  t.notOk(req({m: 0}), 'false no model no huif')
+
+  t.end()
+})
