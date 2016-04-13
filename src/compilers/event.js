f0a13aa4a0c37131cd03d429a6262e7b95ec596e
Jacobo <jacobo@xarope.eu>
2016-04-13 21:44:24 +0200

    Update: refactor events compiler and render

--- src/compilers/event.js
+++ src/compilers/event.js
@@ -1,9 +1,12 @@
 'use strict'
 
 module.exports = function (events) {
+  const keys = Object.keys(events)
+  const map = new Map()
+  keys.forEach(k => map.set(k, events[k]))
+
   return (el, nut) => {
-    Object.keys(events).forEach(k => {
-      let fnName = events[k]
+    map.forEach((fnName, k) => {
       el.addEventListener(k, e => {
         nut[fnName](e, nut)
       })
