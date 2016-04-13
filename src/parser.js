cf48406956d7355f2a722cf3e7c6d41467b25b42
Jacobo <jacobo@xarope.eu>
2016-04-13 03:23:19 +0200

    Update: refactor parser

--- src/parser.js
+++ src/parser.js
@@ -1,52 +1,132 @@
 'use strict'
 
-const props = new Set(['model', 'repeat', 'if', 'unless', 'as', 'hide', 'show'])
+/**
+ * parser
+ *
+ * Parse a nuts template tag and its children into a object.
+ *
+ * List of properties
+ *
+ * - `type` *number*: is the DOM node type (1: element, 3: text)
+ * - `localName` *string*: the localname of the tag
+ * - `tagName` *string*: the name of the template
+ * - `data` *string*: the content of text nodes
+ * - `attribs` *object*: with tag attributes excluding events and nut attributes
+ * - `events` *object*: keys are event names, and values are the callbacks
+ * - `settings` *object*: control properties (model, repeat, if, unless, hide, show, as)
+ * - `children` *array*: children schemas
+ *
+ * Children can have all properties but `tagName`.
+ * Nut templates can have all properties but `model`, `repeat` and `as`
+ */
 
-module.exports = function parser (el, rawChildren) {
-  let atts = {}// attributes
-  let src = {
-    type: el.nodeType,
-    data: el.data,
-    localName: el.localName,
-    attribs: atts
-  }
-  let events = {}
+// template property key names
+const nutProps = new Set(['if', 'unless', 'hide', 'show'])
+const childProps = new Set(['model', 'repeat', 'as', 'if', 'unless', 'hide', 'show'])
+// array methods
+const reduce = Array.prototype.reduce
+const map = Array.prototype.map
 
-  if (el.attributes && el.hasAttributes()) {
-    // fill `atts` object with element attributes
-    Array.prototype.forEach.call(el.attributes, i => {atts[i.name] = i.value})
-    // extract nut name
-    if (atts.nut) {
-      src.tagName = atts.nut
-      delete atts.nut
+// return a regular object with attributes from a element
+function parseAttributes (el) {
+  return reduce.call(el.attributes, (obj, i) => {
+    obj[i.name] = i.value
+    return obj
+  }, {})
+}
+
+function extractProps (atts, propNames) {
+  const props = {}
+  propNames.forEach(prop => {
+    if (prop in atts) {
+      props[prop] = atts[prop]
+      delete atts[prop]
     }
+  })
+  return props
+}
 
-    // extract custom nut properties
-    props.forEach(prop => {
-      if (prop in atts) {
-        src[prop] = atts[prop]
-        delete atts[prop]
-      }
-    })
+function extractEvents (atts) {
+  let events = {}
+  Object.keys(atts).forEach(k => {
+    if (k.startsWith('(') && k.endsWith(')')) {
+      events[k.slice(1, k.length - 1)] = atts[k]
+      delete atts[k]
+    }
+  })
+  return events
+}
 
-    // extract events
-    Object.keys(atts).forEach(k => {
-      if (k.startsWith('(') && k.endsWith(')')) {
-        events[k.slice(1, k.length - 1)] = atts[k]
-        delete atts[k]
-      }
-    })
-    if (Object.keys(events).length) src.events = events
+/**
+ * @param {DOM node} el template tag
+ * @param {boolean} rawChildren indicates whether parse children tags. `false` by default
+ * @returns {object} parsed tag
+ */
+function parseTemplate (el, rawChildren) {
+  const atts = parseAttributes(el)
+
+  // extract nut name
+  const tagName = atts.nut
+  if (!tagName) {
+    throw new Error('templates requires a name (nut attribute)')
   }
+  delete atts.nut
+
+  const src = {
+    type: 1,
+    localName: el.localName,
+    tagName,
+    attribs: atts,
+    props: extractProps(atts, nutProps)
+  }
+
+  // extract events
+  const events = extractEvents(atts)
+  if (Object.keys(events).length) src.events = events
 
   if (rawChildren) {
     src.children = el.childNodes
   } else {
     // parse children dom elements
     if (el.childNodes && el.childNodes.length) {
-      src.children = Array.prototype.map.call(el.childNodes, child => parser(child))
+      src.children = map.call(el.childNodes, child => parseChild(child))
     }
+  }
+  return src
+}
+
+/**
+ * @param {DOM node} el template tag
+ * @returns {object} parsed tag
+ */
+function parseChild (el) {
+  let atts = {}, // attributes
+      props = {}
+
+  let src = {
+    type: el.nodeType,
+    data: el.data,
+    localName: el.localName
+  }
+
+  if (el.attributes && el.hasAttributes()) {
+    // fill `atts` object with element attributes
+    atts = parseAttributes(el)
+
+    // extract custom nut properties
+    src.props = props = extractProps(atts, childProps)
+
+    const events = extractEvents(atts)
+    if (Object.keys(events).length) src.events = events
+  }
+
+  src.attribs = atts
+  // parse children dom elements
+  if (el.childNodes && el.childNodes.length && !props.as) {
+    src.children = map.call(el.childNodes, child => parseChild(child))
   }
 
   return src
 }
+
+module.exports = parseTemplate
--- test/parser-test.js
+++ test/parser-test.js
@@ -7,49 +7,40 @@ function parser (tmpl, rawChildren) {
   return getSource(document.createRange().createContextualFragment(tmpl).childNodes[0], rawChildren)
 }
 
-test('distribute attributes', function (t) {
-  let tmpl = '<span ' +
-        ' nut="specialNuts"' +
-        ' class="{{class}}"' +
-        // scopes
-        ' model="model"' +
-        // conditionals
-        ' if="if"' +
-        ' unless="unless"' +
-        // iterations
-        ' repeat="repeat"' +
-        // layouts
-        ' as="nuas"' +
-        // events
-        ' (keydown)="presskey"' +
-        // events
-        ' (click)="click"' +
-        // regular attributes
-        ' myatt="myatt"' +
-        // variable attribute
-        '>' +
-        'hello' +
-        '</span>',
-      src = parser(tmpl)
+test('distribute attributes in template', function (t) {
+  let tmpl = `<span
+    nut="specialNuts"
+    class="{{class}}"
+    model="model"
+    if="if"
+    unless="unless"
+    repeat="repeat"
+    as="nuas"
+    (keydown)="presskey"
+    (click)="click"
+    myatt="myatt">
+    hello
+    </span>`
+  let src = parser(tmpl)
 
   // class
   t.is(src.localName, 'span')
   t.is(src.attribs.class, '{{class}}')
   // scope
-  t.is(src.model, 'model')
-  t.is(src.attribs.model, undefined)
+  t.notOk(src.props.model)
+  t.is(src.attribs.model, 'model')
   // nuif
-  t.is(src.if, 'if')
+  t.is(src.props.if, 'if')
   t.is(src.attribs.if, undefined)
   // unless
-  t.is(src.unless, 'unless')
+  t.is(src.props.unless, 'unless')
   t.is(src.attribs.unless, undefined)
   // repeat
-  t.is(src.repeat, 'repeat')
-  t.is(src.attribs.repeat, undefined)
+  t.notOk(src.props.repeat)
+  t.is(src.attribs.repeat, 'repeat')
   // as
-  t.is(src.as, 'nuas')
-  t.is(src.attribs.as, undefined)
+  t.notOk(src.props.as)
+  t.is(src.attribs.as, 'nuas')
   // nut keyname
   t.is(src.attribs.nut, undefined)
   t.is(src.tagName, 'specialNuts')
@@ -58,6 +49,107 @@ test('distribute attributes', function (t) {
   // events
   t.is(src.events.click, 'click')
   t.is(src.events.keydown, 'presskey')
+  t.end()
+})
+
+test('distribute attributes in children', function (t) {
+  let tmpl = `<span
+    nut="specialNuts"
+    class="{{class}}"
+    model="model"
+    if="if"
+    unless="unless"
+    repeat="repeat"
+    as="nuas"
+    (keydown)="presskey"
+    (click)="click"
+    myatt="myatt"><span
+      nut="specialNuts"
+      class="{{class}}"
+      model="model"
+      if="if"
+      unless="unless"
+      repeat="repeat"
+      (keydown)="presskey"
+      (click)="click"
+      myatt="myatt"><span
+          nut="specialNuts"
+          class="{{class}}"
+          model="model"
+          if="if"
+          unless="unless"
+          repeat="repeat"
+          as="nuas"
+          (keydown)="presskey"
+          (click)="click"
+          myatt="myatt">hello</span></span><span
+      nut="specialNuts"
+      class="{{class}}"
+      model="model"
+      if="if"
+      unless="unless"
+      repeat="repeat"
+      as="nuas"
+      (keydown)="presskey"
+      (click)="click"
+      myatt="myatt">hello</span></span>`
+
+  let src = parser(tmpl).children[0]
+  // localname
+  t.is(src.localName, 'span', 'localName')
+  // class
+  t.is(src.attribs.class, '{{class}}', 'class')
+  // scope
+  t.is(src.props.model, 'model', 'model')
+  t.is(src.attribs.model, undefined, 'model')
+  // nuif
+  t.is(src.props.if, 'if', 'if')
+  t.is(src.attribs.if, undefined, 'if')
+  // unless
+  t.is(src.props.unless, 'unless', 'unless')
+  t.is(src.attribs.unless, undefined, 'unless')
+  // repeat
+  t.is(src.props.repeat, 'repeat', 'repeat')
+  t.is(src.attribs.repeat, undefined, 'repeat')
+  // nut keyname
+  t.is(src.attribs.nut, 'specialNuts', 'keyname')
+  t.notOk(src.tagName, 'no tagname in children')
+  // regular attributes
+  t.is(src.attribs.myatt, 'myatt')
+  // events
+  t.is(src.events.click, 'click')
+  t.is(src.events.keydown, 'presskey')
+
+  // subchild
+  let child = parser(tmpl).children[0].children[0]
+
+  // class
+  t.is(child.localName, 'span', 'subchild localName')
+  t.is(child.attribs.class, '{{class}}')
+  // scope
+  t.is(child.props.model, 'model')
+  t.is(child.attribs.model, undefined)
+  // as
+  t.is(child.props.as, 'nuas')
+  t.notOk(child.attribs.as, 'nuas')
+  // nuif
+  t.is(child.props.if, 'if')
+  t.is(child.attribs.if, undefined)
+  // unless
+  t.is(child.props.unless, 'unless')
+  t.is(child.attribs.unless, undefined)
+  // repeat
+  t.is(child.props.repeat, 'repeat')
+  t.is(child.attribs.repeat, undefined)
+  // nut keyname
+  t.notOk(child.tagname, 'no tagname in children')
+  t.is(child.attribs.nut, 'specialNuts', 'no tagname in children')
+  // regular attributes
+  t.is(child.attribs.myatt, 'myatt')
+  // events
+  t.is(child.events.click, 'click')
+  t.is(child.events.keydown, 'presskey')
+
   t.end()
 })
 
@@ -78,5 +170,13 @@ test('parse just parent element and send children as raw', function (t) {
       src = parser(tmpl, true)
 
   t.notOk(src.children[0].model)
+  t.end()
+})
+
+test('omit children when tag has `as` attribute', function (t) {
+  let tmpl = '<ul nut="simpleTag"><li as="nuas">hola<span></span></li></ul>',
+      src = parser(tmpl).children[0]
+
+  t.notOk(src.children)
   t.end()
 })
